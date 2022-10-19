279804f78e16c7467f55b4de27ac5469aeaf2020
Nejer <Nejer6@gmail.com>
2022-10-19 03:39:01 +0300

    фильтры и сортировка

--- src/components/app-menu.vue
+++ src/components/app-menu.vue
@@ -20,6 +20,26 @@
           <button @click="searchOrganisms" class="button" style="margin-left: 10px">Искать</button>
         </div>
 
+        <div style="display: flex; justify-content: space-evenly">
+          <div>
+            <p>Царство</p>
+            <select class="button" v-model="kingdom" style="font-size: 12pt">
+              <option>Любое</option>
+              <option>Грибы</option>
+              <option>Животные</option>
+              <option>Растения</option>
+            </select>
+          </div>
+
+          <div>
+            <p>Редкость</p>
+            <select class="button" v-model="sort" style="font-size: 12pt">
+              <option>По убыванию</option>
+              <option>По возрастанию</option>
+            </select>
+          </div>
+        </div>
+
         <div>
           <p class="label"><b>Все: </b></p>
         </div>
@@ -48,8 +68,8 @@ export default {
   name: "app-menu",
 
   emits: [
-      "pushOrganismId",
-      "removeOrganism"
+    "pushOrganismId",
+    "removeOrganism"
   ],
 
   data() {
@@ -65,7 +85,9 @@ export default {
       search: "",
       region: "",
       page: 1,
-      hasNextPage: false
+      hasNextPage: false,
+      kingdom: "Любое",
+      sort: "По убыванию",
     }
   },
 
@@ -78,6 +100,10 @@ export default {
   computed: {
     isEmptySelected() {
       return this.selectedOrganisms.length === 0
+    },
+
+    sortType() {
+      return this.sort === "По убыванию" ? "ASC" : "DESK"
     }
   },
 
@@ -103,7 +129,11 @@ export default {
     async getOrganisms() {
       const count = 10;
       const offset = (this.page - 1) * count
-      const response = await fetch(`http://localhost:8080/api/v1/animals?offset=${offset}&count=${count + 1}&search=${this.search}`);
+      let url = `http://localhost:8080/api/v1/animals?offset=${offset}&count=${count + 1}&search=${this.search}&sort=${this.sortType}`
+      if (this.kingdom !== "Любое") {
+        url += `&kingdom=${this.kingdom}`
+      }
+      const response = await fetch(url);
       const result = await response.json()
 
       if (result.length > count) {
@@ -125,22 +155,30 @@ export default {
   watch: {
     page() {
       this.getOrganisms()
+    },
+
+    kingdom() {
+      this.searchOrganisms()
+    },
+
+    sort() {
+      this.searchOrganisms()
     }
   }
 }
 </script>
 
 <style scoped>
-  .button {
-    padding: 3px;
-    background: #D6AB78;
-    border: 0;
-    border-radius: 3px;
-    font-size: 15pt;
-  }
+.button {
+  padding: 3px;
+  background: #D6AB78;
+  border: 0;
+  border-radius: 3px;
+  font-size: 15pt;
+}
 
-  p.label {
-    font-size: 17pt;
-    margin: 20px 0
-  }
+p.label {
+  font-size: 17pt;
+  margin: 20px 0
+}
 </style>
